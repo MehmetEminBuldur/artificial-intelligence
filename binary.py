45a090e27822c4a41f98e0787b31ce4917a519ce
MehmetEminBuldur <50695488+MehmetEminBuldur@users.noreply.github.com>
2021-10-07 22:47:57 +0300

    character_recognition_with_perceptron_algorithm
    Learning different character fonts with perceptron learning algorithm using binary and bipolar dataset.

---
+++ binary.py
@@ -0,0 +1,205 @@
+#character dataset binary#
+inpa1 = [0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,1,1,1,1,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,1,1,1,0,1,1,1]
+inpb1 = [1,1,1,1,1,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,1,1,1,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,1,1,1,1,1,1,0]
+inpc1 = [0,0,1,1,1,1,1,0,1,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,1,1,1,0]
+inpd1 = [1,1,1,1,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,1,0,1,1,1,1,1,0,0]
+inpe1 = [1, 1,  1,  1,  1,  1,  1, 0, 1, 0, 0, 0, 0,  1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0,  1, 0, 0, 0, 0, 1,  1,  1, 0, 0, 0, 0, 1, 0,  1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,  1, 1, 1,  1,  1,  1,  1,  1]
+inpj1 = [0,0,0,1,1,1,1, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,1,0,0,0,1,0, 0,1,0,0,0,1,0, 0,0,1,1,1,0,0]
+inpk1 = [1,1,1,0,0,1,1,0,1,0,0,1,0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,0,1,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1,0,1,1,1,0,0,1,1]
+inpa2 = [0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0]
+inpb2 = [1,1,1,1,1,1,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,0]
+inpc2 = [0,0,1,1,1,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,1,1,0,0]
+inpd2 = [1,1,1,1,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,1,0,1,1,1,1,1,0,0]
+inpe2 = [1,1,1,1,1,1,1, 1,0,0,0,0,0,0, 1,0,0,0,0,0,0, 1,0,0,0,0,0,0, 1,1,1,1,1,0,0, 1,0,0,0,0,0,0, 1,0,0,0,0,0,0, 1,0,0,0,0,0,0, 1,1,1,1,1,1,1]
+inpj2 = [0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,0,0,0,0,1,0, 0,1,0,0,0,1,0, 0,1,0,0,0,1,0, 0,0,1,1,1,0,0]
+inpk2 = [1,0,0,0,0,1,0,1,0,0,0,1,0,0,1,0,0,1,0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,0,1,0,1,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,1,0]
+inpa3 = [0,0,0,1,0,0,0, 0,0,0,1,0,0,0, 0,0,1,0,1,0,0, 0,0,1,0,1,0,0, 0,1,0,0,0,1,0, 0,1,1,1,1,1,0, 1,0,0,0,0,0,1, 1,0,0,0,0,0,1, 1,1,0,0,0,1,1]
+inpb3 = [1,1,1,1,1,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,1,1,1,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,1,1,1,1,1,1,0]
+inpc3 = [0,0,1,1,1,0,1,0,1,0,0,0,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,1,1,0,0]
+inpd3 = [1,1,1,1,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0,1,0,1,1,1,1,1,0,0]
+inpe3 = [1,1,1,1,1,1,1,0,1,0,0,0,0,1,0,1,0,0,1,0,0,0,1,1,1,1,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1]
+inpj3 = [0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1,1,1,0,0]
+inpk3 = [1,1,1,0,0,1,1,0,1,0,0,0,1,0,0,1,0,0,1,0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,0,1,0,1,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1,0,1,1,1,0,0,1,1]
+oneepo = [ inpa1, inpb1, inpc1, inpd1, inpe1, inpj1, inpk1, inpa2, inpb2, inpc2, inpd2, inpe2, inpj2, inpk2, inpa3, inpb3, inpc3, inpd3, inpe3, inpj3, inpk3]
+#character dataset binary#
+
+#wheights#
+weighta = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightb = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightc = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightd = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weighte = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightj = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightk = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weights = [weighta,weightb,weightc,weightd,weighte,weightj,weightk]#ağırlık matrisi implementasyonu
+#wheights#
+
+targeta = [1,0,0,0,0,0,0]
+targetb = [0,1,0,0,0,0,0]
+targetc = [0,0,1,0,0,0,0]
+targetd = [0,0,0,1,0,0,0]
+targete = [0,0,0,0,1,0,0]
+targetj = [0,0,0,0,0,1,0]
+targetk = [0,0,0,0,0,0,1]
+target = [targeta,targetb,targetc,targetd,targete,targetj,targetk]
+
+count7 = 0#target dizisinde index olarak kullanmak için
+
+lrate = 0.0001
+
+bias = [1,1,1,1,1,1,1]
+
+biasw = [0,0,0,0,0,0,0]
+
+#perceptron#
+for c in range(0,1000): #1000 epoch
+    for arr in oneepo: #1 epochun tamamlanması
+        net = [0,0,0,0,0,0,0] #outputları hesaplamak için
+        for i in range(0,63): #ağırlıklarla 63 inputu çerpıp toplama işlemi
+            net[0] = net[0] + arr[i] * weights[0][i]
+            net[1] = net[1] + arr[i] * weights[1][i]
+            net[2] = net[2] + arr[i] * weights[2][i]
+            net[3] = net[3] + arr[i] * weights[3][i]
+            net[4] = net[4] + arr[i] * weights[4][i]
+            net[5] = net[5] + arr[i] * weights[5][i]
+            net[6] = net[6] + arr[i] * weights[6][i]
+        net[0] = net[0] + bias[0] * biasw[0]
+        net[1] = net[1] + bias[1] * biasw[1]
+        net[2] = net[2] + bias[2] * biasw[2]
+        net[3] = net[3] + bias[3] * biasw[3]
+        net[4] = net[4] + bias[4] * biasw[4]
+        net[5] = net[5] + bias[5] * biasw[5]
+        net[6] = net[6] + bias[6] * biasw[6]
+        print("net  " + str(net))
+        for i in range(0,7): #bipolar threshold fonksiyonu
+            if net[i] > 1:
+                net[i] = 1
+            elif net[i] < 1:
+                net[i] = 0
+        print("output  " + str(net))
+        print("target  " + str(target[count7]))
+        if net == target[count7]: #eğer target ve output uyuşuyorsa değişiklik yapma
+            print("eşitlik")
+        else:
+            for y in range(0,7):
+                for x in range(0,63):#target ve net değer uyuşmuyorsa ağırlıkları güncelle
+                    weights[y][x] = weights[y][x] + lrate * target[count7][y] * arr[x]#arr dizisi input
+                biasw[y] = biasw[y] + lrate * target[count7][y]
+        count7 = count7 + 1
+        if count7 == 7:     #target'a doğru şekilde erişmek için değişken çünkü her 7 tekrarda aynı harf geliyor
+            count7 = 0      #
+print("işlem tamamlandı")
+#perceptron#
+
+#TEST#
+while 1: #kullanıcıdan test için veri alımı
+    opt = input("******Bir harf ve font numarasını şu formatta giriniz--->(1a)********(ÇIKIŞ İÇİN exit YAZIN):")#option
+    if opt == "exit":
+        exit()
+    elif opt == "1a":
+        testarr = inpa1
+        count7 = 0
+    elif opt == "1b":
+        testarr = inpb1
+        count7 = 1
+    elif opt == "1c":
+        testarr = inpc1
+        count7 = 2
+    elif opt == "1d":
+        testarr = inpd1
+        count7 = 3
+    elif opt == "1e":
+        testarr = inpe1
+        count7 = 4
+    elif opt == "1j":
+        testarr = inpj1
+        count7 = 5
+    elif opt == "1k":
+        testarr = inpk1
+        count7 = 6
+    elif opt == "2a":
+        testarr = inpa2
+        count7 = 0
+    elif opt == "2b":
+        testarr = inpb2
+        count7 = 1
+    elif opt == "2c":
+        testarr = inpc2
+        count7 = 2
+    elif opt == "2d":
+        testarr = inpd2
+        count7 = 3
+    elif opt == "2e":
+        testarr = inpe2
+        count7 = 4
+    elif opt == "2j":
+        testarr = inpj2
+        count7 = 5
+    elif opt == "2k":
+        testarr = inpk2
+        count7 = 6
+    elif opt == "3a":
+        testarr = inpa3
+        count7 = 0
+    elif opt == "3b":
+        testarr = inpb3
+        count7 = 1
+    elif opt == "3c":
+        testarr = inpc3
+        count7 = 2
+    elif opt == "3d":
+        testarr = inpd3
+        count7 = 3
+    elif opt == "3e":
+        testarr = inpe3
+        count7 = 4
+    elif opt == "3j":
+        testarr = inpj3
+        count7 = 5
+    elif opt == "3k":
+        testarr = inpk3
+        count7 = 6
+    else:
+        print("hatalı giriş yaptınız.")
+        exit()
+    net = [0,0,0,0,0,0,0]
+    for i in range(0,63): #ağırlıklarla 63 inputu çerpıp toplama işlemi
+        net[0] = net[0] + testarr[i] * weights[0][i]
+        net[1] = net[1] + testarr[i] * weights[1][i]
+        net[2] = net[2] + testarr[i] * weights[2][i]
+        net[3] = net[3] + testarr[i] * weights[3][i]
+        net[4] = net[4] + testarr[i] * weights[4][i]
+        net[5] = net[5] + testarr[i] * weights[5][i]
+        net[6] = net[6] + testarr[i] * weights[6][i]
+    net[0] = net[0] + bias[0] * biasw[0]
+    net[1] = net[1] + bias[1] * biasw[1]
+    net[2] = net[2] + bias[2] * biasw[2]
+    net[3] = net[3] + bias[3] * biasw[3]
+    net[4] = net[4] + bias[4] * biasw[4]
+    net[5] = net[5] + bias[5] * biasw[5]
+    net[6] = net[6] + bias[6] * biasw[6]
+    for i in range(0,7): #bipolar threshold fonksiyonu
+        if net[i] > 1:
+            net[i] = 1
+        elif net[i] < 1:
+            net[i] = 0
+    print(str(net))
+    result = "h"
+    if net == target[6]:# aktivasyon fonksiyonu
+        result  = "K"
+    elif net == target[5]:
+        result  = "J"
+    elif net == target[4]:
+        result  = "E"
+    elif net == target[3]:
+        result  = "D"
+    elif net == target[2]:
+        result  = "C"
+    elif net == target[1]:
+        result  = "B"
+    elif net == target[0]:
+        result  = "A"
+    else:
+        print("hatalı sonuç")
+    print("verilen input " + result + " harfine ait")
+#TEST#
---
+++ bipolar.py
@@ -0,0 +1,208 @@
+#character dataset bipolar#
+inpa1 = [-1,-1,1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,1,1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,1,1,-1,1,1,1]
+inpb1 = [1,1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,1,1,1,1,1,1,-1]
+inpc1 = [-1,-1,1,1,1,1,1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,1,1,1,1,-1]
+inpd1 = [1,1,1,1,1,-1,-1, -1,1,-1,-1,-1,1,-1, -1,1,-1,-1,-1,-1,1, -1,1,-1,-1,-1,-1,1, -1,1,-1,-1,-1,-1,1, -1,1,-1,-1,-1,-1,1, -1,1,-1,-1,-1,-1,1, -1,1,-1,-1,-1,1,-1, 1,1,1,1,1,-1,-1]
+inpe1 = [1, 1,  1,  1,  1,  1,  1, -1, 1, -1, -1, -1, -1,  1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1,  1, -1, -1, -1, -1, 1,  1,  1, -1, -1, -1, -1, 1, -1,  1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1,  1, 1, 1,  1,  1,  1,  1,  1]
+inpj1 = [-1,-1,-1,1,1,1,1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,1,-1,-1,-1,1,-1, -1,1,-1,-1,-1,1,-1, -1,-1,1,1,1,-1,-1]
+inpk1 = [1,1,1,-1,-1,1,1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,1,1,1,-1,-1,1,1]
+inpa2 = [-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,1,1,1,1,1,-1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1]
+inpb2 = [1,1,1,1,1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,-1]
+inpc2 = [-1,-1,1,1,1,-1,-1,-1,1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,-1,-1,-1,1,1,1,-1,-1]
+inpd2 = [1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,-1,-1]
+inpe2 = [1,1,1,1,1,1,1, 1,-1,-1,-1,-1,-1,-1, 1,-1,-1,-1,-1,-1,-1, 1,-1,-1,-1,-1,-1,-1, 1,1,1,1,1,-1,-1, 1,-1,-1,-1,-1,-1,-1, 1,-1,-1,-1,-1,-1,-1, 1,-1,-1,-1,-1,-1,-1, 1,1,1,1,1,1,1]
+inpj2 = [-1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,-1,-1,-1,-1,1,-1, -1,1,-1,-1,-1,1,-1, -1,1,-1,-1,-1,1,-1, -1,-1,1,1,1,-1,-1]
+inpk2 = [1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,-1,1,-1]
+inpa3 = [-1,-1,-1,1,-1,-1,-1, -1,-1,-1,1,-1,-1,-1, -1,-1,1,-1,1,-1,-1, -1,-1,1,-1,1,-1,-1, -1,1,-1,-1,-1,1,-1, -1,1,1,1,1,1,-1, 1,-1,-1,-1,-1,-1,1, 1,-1,-1,-1,-1,-1,1, 1,1,-1,-1,-1,1,1]
+inpb3 = [1,1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,1,1,1,1,1,1,-1]
+inpc3 = [-1,-1,1,1,1,-1,1,-1,1,-1,-1,-1,1,1,1,-1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,-1,-1,-1,1,1,1,-1,-1]
+inpd3 = [1,1,1,1,1,-1,-1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,-1,1,1,1,1,1,-1,-1]
+inpe3 = [1,1,1,1,1,1,1,-1,1,-1,-1,-1,-1,1,-1,1,-1,-1,1,-1,-1,-1,1,1,1,1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,1,1,1,1,1,1,1]
+inpj3 = [-1,-1,-1,-1,1,1,1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,1,1,-1,-1]
+inpk3 = [1,1,1,-1,-1,1,1,-1,1,-1,-1,-1,1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,1,-1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,1,1,1,-1,-1,1,1]
+oneepo = [ inpa1, inpb1, inpc1, inpd1, inpe1, inpj1, inpk1, inpa2, inpb2, inpc2, inpd2, inpe2, inpj2, inpk2, inpa3, inpb3, inpc3, inpd3, inpe3, inpj3, inpk3]#input matrisi implementasyonu
+#character dataset bipolar#
+
+#wheights#
+weighta = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightb = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightc = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightd = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weighte = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightj = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weightk = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+weights = [weighta,weightb,weightc,weightd,weighte,weightj,weightk]#ağırlık matrisi implementasyonu
+#wheights#
+
+targeta = [1,-1,-1,-1,-1,-1,-1]
+targetb = [-1,1,-1,-1,-1,-1,-1]
+targetc = [-1,-1,1,-1,-1,-1,-1]
+targetd = [-1,-1,-1,1,-1,-1,-1]
+targete = [-1,-1,-1,-1,1,-1,-1]
+targetj = [-1,-1,-1,-1,-1,1,-1]
+targetk = [-1,-1,-1,-1,-1,-1,1]
+target = [targeta,targetb,targetc,targetd,targete,targetj,targetk]
+
+count7 = 0#target dizisinde index olarak kullanmak için
+
+lrate = 0.0001
+
+bias = [1,1,1,1,1,1,1]
+
+biasw = [0,0,0,0,0,0,0]
+
+
+#perceptron#
+for c in range(0,1000): #1000 epoch
+    for arr in oneepo: #1 epochun tamamlanması
+        net = [0,0,0,0,0,0,0] #outputları hesaplamak için
+        for i in range(0,63): #ağırlıklarla 63 inputu çerpıp toplama işlemi
+            net[0] = net[0] + arr[i] * weights[0][i]
+            net[1] = net[1] + arr[i] * weights[1][i]
+            net[2] = net[2] + arr[i] * weights[2][i]
+            net[3] = net[3] + arr[i] * weights[3][i]
+            net[4] = net[4] + arr[i] * weights[4][i]
+            net[5] = net[5] + arr[i] * weights[5][i]
+            net[6] = net[6] + arr[i] * weights[6][i]
+        net[0] = net[0] + bias[0] * biasw[0]
+        net[1] = net[1] + bias[1] * biasw[1]
+        net[2] = net[2] + bias[2] * biasw[2]
+        net[3] = net[3] + bias[3] * biasw[3]
+        net[4] = net[4] + bias[4] * biasw[4]
+        net[5] = net[5] + bias[5] * biasw[5]
+        net[6] = net[6] + bias[6] * biasw[6]
+        print("net  " + str(net))
+        for i in range(0,7): #bipolar threshold fonksiyonu
+            if net[i] > 1:
+                net[i] = 1
+            elif net[i] < -1:
+                net[i] = -1
+            else:
+                net[i] = 0
+        print("output  " + str(net))
+        print("target  " + str(target[count7]))
+        if net == target[count7]: #eğer target ve output uyuşuyorsa değişiklik yapma
+            print("eşitlik")
+        else:
+            for y in range(0,7):
+                for x in range(0,63):#target ve net değer uyuşmuyorsa ağırlıkları güncelle
+                    weights[y][x] = weights[y][x] + lrate * target[count7][y] * arr[x]#arr dizisi input
+                biasw[y] = biasw[y] + lrate * target[count7][y]
+        count7 = count7 + 1
+        if count7 == 7:     #target'a doğru şekilde erişmek için değişken çünkü her 7 tekrarda aynı harf geliyor
+            count7 = 0      #
+print("işlem tamamlandı")
+#perceptron#
+
+#test#
+while 1: #kullanıcıdan test için veri alımı
+    opt = input("******Bir harf ve font numarasını şu formatta giriniz--->(1a)********(ÇIKIŞ İÇİN exit YAZIN):")#option
+    if opt == "exit":
+        exit()
+    elif opt == "1a":
+        testarr = inpa1
+        count7 = 0
+    elif opt == "1b":
+        testarr = inpb1
+        count7 = 1
+    elif opt == "1c":
+        testarr = inpc1
+        count7 = 2
+    elif opt == "1d":
+        testarr = inpd1
+        count7 = 3
+    elif opt == "1e":
+        testarr = inpe1
+        count7 = 4
+    elif opt == "1j":
+        testarr = inpj1
+        count7 = 5
+    elif opt == "1k":
+        testarr = inpk1
+        count7 = 6
+    elif opt == "2a":
+        testarr = inpa2
+        count7 = 0
+    elif opt == "2b":
+        testarr = inpb2
+        count7 = 1
+    elif opt == "2c":
+        testarr = inpc2
+        count7 = 2
+    elif opt == "2d":
+        testarr = inpd2
+        count7 = 3
+    elif opt == "2e":
+        testarr = inpe2
+        count7 = 4
+    elif opt == "2j":
+        testarr = inpj2
+        count7 = 5
+    elif opt == "2k":
+        testarr = inpk2
+        count7 = 6
+    elif opt == "3a":
+        testarr = inpa3
+        count7 = 0
+    elif opt == "3b":
+        testarr = inpb3
+        count7 = 1
+    elif opt == "3c":
+        testarr = inpc3
+        count7 = 2
+    elif opt == "3d":
+        testarr = inpd3
+        count7 = 3
+    elif opt == "3e":
+        testarr = inpe3
+        count7 = 4
+    elif opt == "3j":
+        testarr = inpj3
+        count7 = 5
+    elif opt == "3k":
+        testarr = inpk3
+        count7 = 6
+    else:
+        print("hatalı giriş yaptınız.")
+        exit()
+    net = [0,0,0,0,0,0,0]
+    for i in range(0,63): #ağırlıklarla 63 inputu çerpıp toplama işlemi
+        net[0] = net[0] + testarr[i] * weights[0][i]
+        net[1] = net[1] + testarr[i] * weights[1][i]
+        net[2] = net[2] + testarr[i] * weights[2][i]
+        net[3] = net[3] + testarr[i] * weights[3][i]
+        net[4] = net[4] + testarr[i] * weights[4][i]
+        net[5] = net[5] + testarr[i] * weights[5][i]
+        net[6] = net[6] + testarr[i] * weights[6][i]
+    net[0] = net[0] + bias[0] * biasw[0]
+    net[1] = net[1] + bias[1] * biasw[1]
+    net[2] = net[2] + bias[2] * biasw[2]
+    net[3] = net[3] + bias[3] * biasw[3]
+    net[4] = net[4] + bias[4] * biasw[4]
+    net[5] = net[5] + bias[5] * biasw[5]
+    net[6] = net[6] + bias[6] * biasw[6]
+    for i in range(0,7): #bipolar threshold fonksiyonu
+        if net[i] > 1:
+            net[i] = 1
+        elif net[i] < -1:
+            net[i] = -1
+        else:
+            net[i] = 0
+    print(str(net))
+    if net == target[6]:# aktivasyon fonksiyonu
+        print("verilen input K harfine ait")
+    elif net == target[5]:
+        print("verilen input J harfine ait")
+    elif net == target[4]:
+        print("verilen input E harfine ait")
+    elif net == target[3]:
+        print("verilen input D harfine ait")
+    elif net == target[2]:
+        print("verilen input C harfine ait")
+    elif net == target[1]:
+        print("verilen input B harfine ait")
+    elif net == target[0]:
+        print("verilen input A harfine ait")
+    else:
+        print("hatalı sonuç")
+#test#
